c8e1344664bd0929659387bf11f927840893a9d4
secxndary <valdaitsevv@mail.ru>
2022-09-27 21:30:59 +0300

    Lab 2 — tasks 1,2

---
+++ PBSP_Lab2/PBSP_Lab2/PBSP_Lab2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include "Winsock2.h"
+#pragma comment (lib, "WS2_32.lib")
+using namespace std;
+
+
+
+
+string  GetErrorMsgText(int code)    // cформировать текст ошибки 
+{
+	string msgText;
+	switch (code)                      // проверка кода возврата  
+	{
+	case WSAEINTR:          msgText = "WSAEINTR";         break;
+	case WSAEACCES:         msgText = "WSAEACCES";        break;
+		//..........коды WSAGetLastError ..........................
+	case WSASYSCALLFAILURE: msgText = "WSASYSCALLFAILURE"; break;
+	default:                msgText = "***ERROR***";      break;
+	};
+	return msgText;
+};
+
+
+
+string SetErrorMsgText(string msgText, int code)
+{
+	return msgText + GetErrorMsgText(code);
+}
+
+
+
+
+
+int main()
+{
+	SOCKET sS = socket(AF_INET, SOCK_STREAM, NULL);
+	try
+	{
+		//............................................................
+		if ((sS = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
+			throw  SetErrorMsgText("socket:", WSAGetLastError());
+		//.............................................................
+	}
+	catch (string errorMsgText)
+	{
+		cout << endl << "WSAGetLastError: " << errorMsgText;
+	}
+
+
+}
---
+++ PBSP_Lab2/PBSP_Lab2/SetErrorMsgText.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Winsock2.h"
+#pragma comment (lib, "WS2_32.lib")
+using namespace std;
+
+
+string SetErrorMsgText(string msgText, int code)
+{
+	return  msgText + GetErrorMsgText(code);
+};
+
+
+string  GetErrorMsgText(int code)    // cформировать текст ошибки 
+{
+	string msgText;
+	switch (code)                      // проверка кода возврата  
+	{
+	case WSAEINTR:          msgText = "WSAEINTR";         break;
+	case WSAEACCES:         msgText = "WSAEACCES";        break;
+		//..........коды WSAGetLastError ..........................
+	case WSASYSCALLFAILURE: msgText = "WSASYSCALLFAILURE"; break;
+	default:                msgText = "***ERROR***";      break;
+	};
+	return msgText;
+};
