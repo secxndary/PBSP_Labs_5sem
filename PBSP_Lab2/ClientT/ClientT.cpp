8ff45a545b7f8855687f0d58c9e0b77f214dc1cd
secxndary <valdaitsevv@mail.ru>
2022-09-28 12:27:15 +0300

    Lab 3 — task 7

--- PBSP_Lab2/ClientT/ClientT.cpp
+++ PBSP_Lab2/ClientT/ClientT.cpp
@@ -1,20 +1,54 @@
-﻿// ClientT.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include "Winsock2.h"
+#pragma comment (lib, "WS2_32.lib")
+using namespace std;
 
-#include <iostream>
+
+
+string  GetErrorMsgText(int code)    // cформировать текст ошибки 
+{
+	string msgText;
+	switch (code)                      // проверка кода возврата  
+	{
+	case WSAEINTR:          msgText = "WSAEINTR";         break;
+	case WSAEACCES:         msgText = "WSAEACCES";        break;
+		//..........коды WSAGetLastError ..........................
+	case WSASYSCALLFAILURE: msgText = "WSASYSCALLFAILURE"; break;
+	default:                msgText = "***ERROR***";      break;
+	};
+	return msgText;
+};
+
+
+
+string SetErrorMsgText(string msgText, int code)
+{
+	return msgText + GetErrorMsgText(code);
+}
+
+
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SOCKET  sS;           // дескриптор сокета 
+	WSADATA wsaData;
+
+	try
+	{
+
+		if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+			throw  SetErrorMsgText("Startup:", WSAGetLastError());
+		if ((sS = socket(AF_INET, SOCK_STREAM, NULL)) == INVALID_SOCKET)
+			throw  SetErrorMsgText("socket:", WSAGetLastError());
+		if (closesocket(sS) == SOCKET_ERROR)
+			throw  SetErrorMsgText("closesocket:", WSAGetLastError());
+		if (WSACleanup() == SOCKET_ERROR)
+			throw  SetErrorMsgText("Cleanup:", WSAGetLastError());
+	}
+	catch (string errorMsgText)
+	{
+		cout << endl << "WSAGetLastError: " << errorMsgText;
+	}
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
