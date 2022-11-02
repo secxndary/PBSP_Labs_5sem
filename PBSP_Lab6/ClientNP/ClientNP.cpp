e28363678cbbb698b4be92cbf345c84de869931c
secxndary <valdaitsevv@mail.ru>
2022-11-02 21:02:17 +0300

    Lab 6 — task 6

--- PBSP_Lab6/ClientNP/ClientNP.cpp
+++ PBSP_Lab6/ClientNP/ClientNP.cpp
@@ -5,6 +5,7 @@
 #pragma comment (lib, "WS2_32.lib")
 #pragma warning(disable:4996)
 #define PIPE_NAME L"\\\\.\\pipe\\Tube"
+#define PIPE_NAME_LAN L"\\\\DESKTOP-8HNL9IM\\pipe\\Tube"	
 #define STOP "STOP"
 using namespace std;
 
@@ -29,6 +30,8 @@ int main()
 	{
 		// 1.
 		if ((cH = CreateFile(
+			// для передачи в локальной сети: на сервере оставьте такой же локальный PIPE_NAME, 
+			// а на клиента замените на его PIPE_NAME_LAN и введите в него имя своего серверного компа
 			PIPE_NAME,								// [in] символическое имя канала 
 			GENERIC_READ | GENERIC_WRITE,			// [in] чтение или запись в канал 
 			FILE_SHARE_READ | FILE_SHARE_WRITE,		// [in] режим совместного использования
